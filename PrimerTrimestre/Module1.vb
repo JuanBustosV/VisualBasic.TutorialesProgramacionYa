40c1689247461e580c55db2aa5c0111f022b2649
JuanBustosV <juanbv81@gmail.com>
2019-07-27 23:05:55 +0200

    Ejemplos tema 8

---
+++ PrimerTrimestre/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+#Region "Método Main"
+    ''' <summary>
+    ''' Se carga una fecha (día, mes y año) por teclado. Mostrar un mensaje si corresponde al primer trimestre del año (enero, febrero o marzo) Cargar por teclado el valor numérico del día, mes y año.
+    ''' Ejemplo: dia:10 mes:1 año:2016. 
+    ''' </summary>
+    Sub Main()
+        Console.Title = "8 - Operador Or"
+
+        Dim dia, mes, año As Integer
+
+        Console.Write("Ingrese nro de día:")
+        dia = Console.ReadLine()
+        Console.Write("Ingrese nro de mes:")
+        mes = Console.ReadLine()
+        Console.Write("Ingrese nro de año:")
+        año = Console.ReadLine()
+
+        If mes = 1 Or mes = 2 Or mes = 3 Then
+            Console.Write("Corresponde al primer trimestre")
+        End If
+        Console.ReadKey()
+    End Sub
+#End Region
+End Module
---
+++ Tema08/MayorTresNumeros/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+#Region "Método Main"
+    ''' <summary>
+    ''' Confeccionar un programa que lea por teclado tres números distintos y nos muestre el mayor.
+    ''' </summary>
+    Sub Main()
+        Console.Title = "8 - Estructuras condicionales con condiciones compuestas y operadores lógicos"
+
+        Dim num1, num2, num3 As Integer
+        Console.Write("Ingrese primer valor:")
+        num1 = Console.ReadLine()
+        Console.Write("Ingrese segundo valor:")
+        num2 = Console.ReadLine()
+        Console.Write("Ingrese tercer valor:")
+        num3 = Console.ReadLine()
+        If num1 > num2 And num1 > num3 Then
+            Console.Write(num1)
+        Else
+            If num2 > num3 Then
+                Console.Write(num2)
+            Else
+                Console.Write(num3)
+            End If
+        End If
+        Console.ReadKey()
+    End Sub
+#End Region
+End Module
