e98309306c61ef4de18fedc4987e51f5364cdd4b
JuanBustosV <juanbv81@gmail.com>
2019-08-30 17:27:14 +0200

    Mejoras 08, Primer trimestre

--- PrimerTrimestre/Module1.vb
+++ PrimerTrimestre/Module1.vb
@@ -9,16 +9,18 @@
 
         Dim dia, mes, año As Integer
 
-        Console.Write("Ingrese nro de día:")
+        Console.Write("Ingrese nro de día: ")
         dia = Console.ReadLine()
-        Console.Write("Ingrese nro de mes:")
+        Console.Write("Ingrese nro de mes: ")
         mes = Console.ReadLine()
-        Console.Write("Ingrese nro de año:")
+        Console.Write("Ingrese nro de año: ")
         año = Console.ReadLine()
 
         If mes = 1 Or mes = 2 Or mes = 3 Then
             Console.Write("Corresponde al primer trimestre")
         End If
+
+        Console.WriteLine(vbNewLine & "Presione cualquier tecla para terminar...")
         Console.ReadKey()
     End Sub
 #End Region
