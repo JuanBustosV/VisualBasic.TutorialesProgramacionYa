cfca8cbcbad9af8e9bd9198e98ac1c598eff78c4
JuanBustosV <juanbv81@gmail.com>
2019-09-01 11:41:22 +0200

    Tema8, problemas 2 y 3

--- Tema08/Problemas/Problema2/Module1.vb
+++ Tema08/Problemas/Problema2/Module1.vb
@@ -1,7 +1,37 @@
 ﻿Module Module1
+    ''' <summary>
+    ''' Se ingresan tres numes por teclado, si todos son iguales se imprime la suma del primero con el segundo y a este resultado se lo multiplica por el tercero.
+    ''' </summary>
 
+#Region "main"
     Sub Main()
+        Console.Title = "PROBLEMA 2 (8 - Estructuras condicionales con condiciones compuestas y operadores lógicos)"
 
+        Dim num1, num2, num3 As Integer
+
+        Console.Write("Ingrese primer valor: ")
+        num1 = Console.ReadLine()
+
+        Console.Write("Ingrese segundo valor: ")
+        num2 = Console.ReadLine()
+
+        Console.Write("Ingrese tercer valor: ")
+        num3 = Console.ReadLine()
+
+        If num1 = num2 AndAlso num1 = num3 Then
+            Dim suma, producto As Integer
+            suma = num1 + num2
+            Console.Write(vbNewLine & "La suma del primero y segundo: ")
+            Console.WriteLine(suma)
+            producto = suma * num3
+            Console.Write("La suma del primero y segundo multiplicado por el tercero: ")
+            Console.WriteLine(producto)
+        End If
+
+        Console.WriteLine(vbNewLine & "Presione cualquier tecla para salir...")
+        Console.ReadKey()
     End Sub
+
+#End Region
 
 End Module
---
+++ Tema08/Problemas/Problema3/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+    ''' <summary>
+    ''' Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10, imprimir en pantalla la leyenda "Todos los números son menores a diez".
+    ''' </summary>
+
+#Region "main"
+    Sub Main()
+        Console.Title = "PROBLEMA 3 (8 - Estructuras condicionales con condiciones compuestas y operadores lógicos)"
+
+        Dim num1, num2, num3 As Integer
+
+        Console.Write("Ingrese primer valor: ")
+        num1 = Console.ReadLine()
+
+        Console.Write("Ingrese segundo valor: ")
+        num2 = Console.ReadLine()
+
+        Console.Write("Ingrese tercer valor: ")
+        num3 = Console.ReadLine()
+
+        If num1 < 10 AndAlso num2 < 10 AndAlso num3 < 10 Then
+            Console.WriteLine(vbNewLine & "Todos los números son menores a diez")
+        End If
+
+        Console.WriteLine(vbNewLine & "Presione cualquier tecla para salir...")
+        Console.ReadKey()
+    End Sub
+
+#End Region
+
+End Module
