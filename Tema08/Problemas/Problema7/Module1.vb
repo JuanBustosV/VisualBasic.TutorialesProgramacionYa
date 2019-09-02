8f45cdb398c1b8b4d5817ec6c4d064c5645c412f
JuanBustosV <juanbv81@gmail.com>
2019-09-02 16:37:24 +0200

    ültimo problema tema8, empezar tema 9

--- Tema08/Problemas/Problema7/Module1.vb
+++ Tema08/Problemas/Problema7/Module1.vb
@@ -7,6 +7,42 @@
     Sub Main()
         Console.Title = "PROBLEMA 7 (8 - Estructuras condicionales con condiciones compuestas y operadores lógicos)"
 
+        Dim num1, num2, num3 As Integer
+
+        Console.Write("Ingrese primer valor: ")
+        num1 = Console.ReadLine()
+
+        Console.Write("Ingrese segundo valor: ")
+        num2 = Console.ReadLine()
+
+        Console.Write("Ingrese tercer valor: ")
+        num3 = Console.ReadLine()
+
+        Console.Write(vbNewLine & "Rango de valores: ")
+
+        'Obtengo el mínimo valor
+        If num1 < num2 AndAlso num1 < num3 Then
+            Console.Write(num1)
+        Else
+            If num2 < num3 Then
+                Console.Write(num2)
+            Else
+                Console.Write(num3)
+            End If
+        End If
+
+        Console.Write("-")
+        'Obtengo el máximo valor
+        If num1 > num2 AndAlso num1 > num3 Then
+            Console.WriteLine(num1)
+        Else
+            If num2 > num3 Then
+                Console.WriteLine(num2)
+            Else
+                Console.WriteLine(num3)
+            End If
+        End If
+
         Console.WriteLine(vbNewLine & "Presione cualquier tecla para salir...")
         Console.ReadKey()
     End Sub
