93b6a91f2ae004a6a26b652d903d08f8dc5cd74d
JuanBustosV <juanbv81@gmail.com>
2019-09-05 17:14:08 +0200

    Problema propuesto 1, Tema 9

--- Tema09/Problemas/Problema1/Module1.vb
+++ Tema09/Problemas/Problema1/Module1.vb
@@ -6,7 +6,32 @@
 #Region "main"
 
     Sub Main()
+        Console.Title = "PROBLEMA 1 (9 - Estructura repetitiva Do While (condición) ... Loop)"
 
+        Dim nota As Single
+        Dim notasMayorISiete As Integer = 0
+        Dim notasMenorSiete As Integer = 0
+        Dim i As Integer = 0
+
+        Console.WriteLine()
+        Do While (i < 10)
+            Console.Write("Ingrese nota {0}: ", i + 1)
+            nota = Console.ReadLine()
+
+            If nota >= 7 Then
+                notasMayorISiete += 1
+            Else
+                notasMenorSiete += 1
+            End If
+
+            i += 1
+        Loop
+
+        Console.Write(vbNewLine & "Cantidad de alumnos con notas mayores o iguales a 7: ")
+        Console.WriteLine(notasMayorISiete)
+        Console.Write("Cantidad de alumons con notas menores a 7: ")
+        Console.Write(notasMenorSiete)
+        Console.ReadKey()
     End Sub
 
 #End Region
---
+++ Tema09/Problemas/Problema2/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+    ''' <summary>
+    ''' Se ingresan un conjunto de n alturas de personas por teclado. Mostrar la altura promedio de las personas.
+    ''' </summary>
+
+#Region "main"
+
+    Sub Main()
+        Console.Title = "PROBLEMA 2 (9 - Estructura repetitiva Do While (condición) ... Loop)"
+
+
+
+        Console.ReadKey()
+    End Sub
+
+#End Region
+
+End Module
